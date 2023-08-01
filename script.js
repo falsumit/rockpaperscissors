9ba7999ad36592a6dbcecc9eb3b4bd9b8d745731
falsumit <brooks516@outlook.com>
2023-08-01 16:31:52 -0400

    Massive overhaul to game code.

--- script.js
+++ script.js
@@ -1,101 +1,46 @@
-function playRound(playerSelection, computerSelection) {
-    function playerChoice() {
-        let playerEntry = prompt("Choose a hand.");
-        return playerEntry.charAt(0).toUpperCase() + playerEntry.substring(1).toLowerCase();
-    }
-    playerSelection = playerChoice();
-    console.log("You chose " + playerSelection);
+const choices = ["Rock", "Paper", "Scissors"];
+const playerSelection = prompt("Choose rock, paper, or scissors");
+let i = 0;
+let playerHand;
+let cpuHand;
+let playerScore = 0;
+let cpuScore = 0;
 
-    function computerChoice() {
-        let handChoices = ["Rock", "Paper", "Scissors"];
-        handResult = Math.floor(Math.random() * handChoices.length);
-        //console.log(handChoices[handResult]);
-        return handChoices[handResult]
+for (const choice of choices) {
+    playerHand = playerSelection.toLowerCase();
+    if (choice.toLowerCase() === playerHand) {
+        playerHand = choice;
+        console.log(`You chose ${choice}.`);
+        i++
+        break;
     }
-    computerSelection = computerChoice();
-    console.log("Your opponent chose " + computerSelection);
+} if (i < 1) {
+    console.log("Choose again.");
+}
 
-    if (playerSelection == computerSelection) {
-        console.log("Draw. Choose again.");
-    }
-    if (playerSelection == "Rock" && computerSelection == "Scissors" ||
-        playerSelection == "Paper" && computerSelection == "Rock" ||
-        playerSelection == "Scissors" && computerSelection == "Paper") {
+function cpuChoice() {
+    cpuHand = choices[(Math.floor(Math.random() * choices.length))];
+} if (i > 0) {
+    cpuChoice();
+    console.log(`Your oppenent chose ${cpuHand}.`);
+    i++
+}
+
+if (i > 1) {
+    if (playerHand === cpuHand) {
+        console.log(`Tie! You both chose ${playerHand}.`);
+    } else if (playerHand == "Rock" && cpuHand == "Paper" ||
+        playerHand == "Paper" && cpuHand == "Scissors" ||
+        playerHand == "Scissors" && cpuHand == "Rock") {
+            console.log("You lose!");
+            cpuScore++
+    } else {
         console.log("You win!");
+        playerScore++
     }
-    if (playerSelection == "Rock" && computerSelection == "Paper" ||
-        playerSelection == "Paper" && computerSelection == "Scissors" ||
-        playerSelection == "Scissors" && computerSelection == "Rock") {
-        console.log("You lose!");
-    }
+    i++
+} if (i > 2) {
+    console.log(`The current score is:
+You: ${playerScore}
+CPU: ${cpuScore}`);
 }
-
-playRound();
-
-/*
-OLD ANSWER
-
-// computer
-let compAnswer = "";
-
-function compHandConvert() {
-    compAnswer = Math.floor(Math.random() * 3);
-    if (compAnswer == 0) {
-        return "Rock";
-    } else if (compAnswer == 1) {
-        return "Paper"
-    } else {
-        return "Scissors"
-    }
-}
-
-let compHand = compHandConvert();
-
-
-// player
-let handPrompt = "Please choose rock, paper, or scissors.";
-let userAnswer = prompt(handPrompt);
-
-function playerHandCapitalize(correctedAnswer = userAnswer) {
-    return correctedAnswer.charAt(0).toUpperCase() + userAnswer.substring(1).toLowerCase()
-}
-
-let playerHand = playerHandCapitalize();
-
-let userTest = 0;
-while (userTest == 0) {
-    if(playerHand === "Rock" || playerHand === "Paper" || playerHand === "Scissors") {
-        userTest = 1;
-    } else {
-        alert("Please choose rock, paper, or scissors.");
-        userTest = 2;
-    }
-}
-
-
-// result comparison
-function gameResult() {
-    if (compHand == playerHand) {
-        console.log("You both chose the same hand. Please choose again.");
-    } else if (compHand == "Scissors" && playerHand == "Paper") {
-        console.log("You lose. Please try again.")
-    } else if (compHand == "Rock" && playerHand == "Scissors") {
-        console.log("You lose. Please try again.")
-    } else if (compHand == "Paper" && playerHand == "Rock") {
-        console.log("You lose. Please try again.")
-    } else if (compHand == "Paper" && playerHand == "Scissors") {
-        console.log("You win. Congratulations!")
-    } else if (compHand == "Scissors" && playerHand == "Rock") {
-        console.log("You win. Congratulations!")
-    } else if (compHand == "Rock" && playerHand == "Paper") {
-        console.log("You win. Congratulations!")
-    } else {
-        console.log("Please reload to try again.");
-    }
-}
-
-// print result
-console.log("You chose " + playerHand);
-console.log("Your opponent chose " + compHand);
-gameResult();
-*/
